89bef52517e464f7e912e936b4b0267714bdcb15
2dam <2dam@HZ375759.TartangaLH.eus>
2024-09-18 10:54:20 +0200

    Funcionales boton de apertura de calculadora y cierre

--- src/psp_procesos_ejercicio5_2/FXMLDocumentController.java
+++ src/psp_procesos_ejercicio5_2/FXMLDocumentController.java
@@ -1,35 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package psp_procesos_ejercicio5_2;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
-/**
- *
- * @author 2dam
- */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController {
 
     @FXML
-    private Label label;
+    private Button openCalcButton;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button closeCalcButton;
+
+    private Process calculatorProcess;
+
+    @FXML
+    private void handleOpenCalc(ActionEvent event) throws IOException {
+        // Iniciar la calculadora
+        if (calculatorProcess == null || !calculatorProcess.isAlive()) {
+            ProcessBuilder builder = new ProcessBuilder("calc.exe");
+            calculatorProcess = builder.start();
+        }
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    @FXML
+    private void handleCloseCalc(ActionEvent event) {
+        try {
+            // Usa el nombre del proceso en lugar del PID
+            ProcessBuilder builder = new ProcessBuilder("taskkill", "/IM", "CalculatorApp.exe", "/F");
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);  // Muestra la salida del comando
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+
+    private String getCalculatorPID() throws IOException {
+        ProcessBuilder builder = new ProcessBuilder("tasklist", "/FI", "IMAGENAME eq calc.exe", "/FO", "CSV");
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("calc.exe")) {
+                String[] parts = line.split(",");
+                return parts[1].replaceAll("\"", "");  // Extraer el PID
+            }
+        }
+        return null;  // No se encontró el PID
+    }
 }
--- src/psp_procesos_ejercicio5_2/PSP_Procesos_Ejercicio5_2.java
+++ src/psp_procesos_ejercicio5_2/PSP_Procesos_Ejercicio5_2.java
@@ -1,36 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package psp_procesos_ejercicio5_2;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author 2dam
- */
-public class PSP_Procesos_Ejercicio5_2 extends javafx.application.Application {
+
+
+public class PSP_Procesos_Ejercicio5_2 extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Calculator Control");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-
 }
