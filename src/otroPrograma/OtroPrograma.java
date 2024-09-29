56266ac4bfdb7ea89d43ae93792bafd16dd148b4
Urkopf <urkopf@gmail.com>
2024-09-29 13:45:23 +0200

    Añadido otroPrograma

---
+++ src/otroPrograma/OtroPrograma.java
@@ -0,0 +1,24 @@
+package otroPrograma;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class OtroPrograma extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        primaryStage.setTitle("Calculator Control");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
